d09a09d9e44cd6c7552538524569e0b475cbf882
Haitam Elgharras <elgharras.haitam@gmail.com>
2024-01-29 20:07:39 +0100

    create a header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `<h1>Header Component</h1>`,
+  styles: ['h1{color:blue}'],
+})
+export class HeaderComponent {}
