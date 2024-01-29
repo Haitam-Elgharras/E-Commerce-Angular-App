2f9b9726b8aebecc745ded91d859926f4260623b
Haitam Elgharras <elgharras.haitam@gmail.com>
2024-01-29 21:48:56 +0100

    global styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `<h1>Header Component</h1>`,
-  styles: ['h1{color:blue}'],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {}
